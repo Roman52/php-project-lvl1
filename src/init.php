f99d5ec538225378377adb2ba96bd2cfe6114751
Roman Zharov <rzharov.dev@gmail.com>
2025-12-16 15:49:45 +0300

    update autoloader path

--- src/init.php
+++ src/init.php
@@ -1,6 +1,6 @@
 <?php
 
-if (! file_exists($composer = 'vendor/autoload.php')) {
+if (! file_exists($composer = __DIR__ . '/../vendor/autoload.php')) {
     die('Error locating autoloader. Please run <code>make install</code>.');
 }
 require_once $composer;
