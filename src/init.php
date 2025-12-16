0b70b3a5d0c20fd6bf9658c421904837d9d734f5
Roman Zharov <rzharov.dev@gmail.com>
2025-12-16 15:58:12 +0300

    autoloader add global path

--- src/init.php
+++ src/init.php
@@ -1,6 +1,10 @@
 <?php
 
-if (! file_exists($composer = __DIR__ . '/../vendor/autoload.php')) {
-    die('Error locating autoloader. Please run <code>make install</code>.');
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
 }
-require_once $composer;
